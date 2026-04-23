50cc6dcf4fe1f1f60db756e5140993308c80f7b4
WillianServello <rafaelwillian299@gmail.com>
2026-04-23 20:16:10 -0300

    feat: ImplementandoDesing (desenvolvendo)

--- kybe.presentation/obj/Debug/net8.0/kybe.presentation.AssemblyInfo.cs
+++ kybe.presentation/obj/Debug/net8.0/kybe.presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kybe.presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc16aba202fb54c7f44c264858e7e0d9685b533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c907688040c2a9df0b1db386e0627a570e2faac")]
 [assembly: System.Reflection.AssemblyProductAttribute("kybe.presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kybe.presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

7538755b5f4982381f1eda66ee65e1383effdb75
WillianServello <rafaelwillian299@gmail.com>
2026-05-19 22:22:24 -0300

    feat: ImplementandoDesign (login)

---
+++ kybe.presentation/ViewModels/Entity/User/Login/UserLoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kybe.presentation.ViewModels.Entity.User.Login
+{
+    public sealed class UserLoginVM
+    {
+        [Required(ErrorMessage = "Informe o usuário")]
+        public string UserName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Informe a senha")]
+        public string Password { get; set; } = string.Empty;
+    }
+}

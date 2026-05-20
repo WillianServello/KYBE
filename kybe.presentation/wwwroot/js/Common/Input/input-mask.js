595b16300e58edd17b573a12fa659f30cc487da0
WillianServello <rafaelwillian299@gmail.com>
2026-05-20 20:05:39 -0300

    feat: Implementando-InputMask & Validação-CPF

---
+++ kybe.presentation/wwwroot/js/Common/Input/input-mask.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    $("#mask-cpf").inputmask({
+        mask: "999.999.999-99",
+        showMaskOnFocus: true,
+        showMaskOnHover: false
+    });
+
+    $("#mask-phone").inputmask({
+        mask: "(99) 99999-9999",
+        showMaskOnFocus: true,   
+        showMaskOnHover: false   
+    });
+
+});
+
---
+++ kybe.presentation/wwwroot/js/Common/Input/input-validation.js
@@ -0,0 +1,25 @@
+﻿function CpfValidation(cpf) {
+    cpf = cpf.replace(/\D/g, '');
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+    let soma = 0, resto;
+
+    for (let i = 1; i <= 9; i++) soma += parseInt(cpf[i - 1]) * (11 - i);
+    resto = (soma * 10) % 11; if (resto === 10) resto = 0;
+    if (resto !== parseInt(cpf[9])) return false;
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++) soma += parseInt(cpf[i - 1]) * (12 - i);
+    resto = (soma * 10) % 11; if (resto === 10) resto = 0;
+    if (resto !== parseInt(cpf[10])) return false;
+
+    return true;
+}
+
+$.validator.addMethod("cpfvalido", function (value, element) {
+    return this.optional(element) || CpfValidation(value);
+}, "CPF inválido");
+
+$(function () {
+    $("#mask-cpf").rules("add", { cpfvalido: true });
+});
